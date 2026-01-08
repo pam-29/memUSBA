e1de51d89d0ebf399651b67e2292225c75bc8cd2
Sara <sararezhimand@gmail.com>
2026-01-08 12:42:53 +0100

    feat: create:home/create-meme

---
+++ resources/views/create.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/create.css">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Choisis ton tableau</h1>
+    
+    <!-- <div>slideshow</div> -->
+
+    <label for="name">Ecris ton commentaire</label>
+    <input type="text" id="name">
+    <a href="" class="button"> VALIDER </a>
+    <a href="" class="button"> VOTER SANS CRÉER </a>
+
+</body>
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/home.css">
+    <title>MemUSBA</title>
+</head>
+<body>
+    <h1>
+        Bienvenue sur MemUSBA !
+    </h1>
+
+    <p>Viens créer ton propre meme et atteindre le podium</p>
+
+    <a href="" class="button"> 
+        COMMENCER
+    </a>
+
+</body>
+</html> 
