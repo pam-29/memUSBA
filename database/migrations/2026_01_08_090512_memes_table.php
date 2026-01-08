c3e6b7d1512f5eefb69e4f7086d40b7243cd2153
pam-29 <pamelaarijaona@gmail.com>
2026-01-08 11:01:21 +0100

    feat: create tables: memes, portrait, banned-words

---
+++ database/migrations/2026_01_08_090512_memes_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('memes', function (Blueprint $table) {
+            $table->id();
+            $table->string('text');
+            $table->integer('likes')->default(0);
+            $table->integer('view')->default(0);
+            $table->foreignId('portrait_id')->constrained('portraits');
+            $table->timestamps();
+            $table->boolean('public');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('memes');
+    }
+};
